3487f133a466e327429702607ac65fc77848941b
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-12-15 13:16:37 +0200

    rewriting task nr -> 345 -> 5/75-Study plan

--- LeetCode75/easy/Task345_ReverseVowelsOfAString.java
+++ LeetCode75/easy/Task345_ReverseVowelsOfAString.java
@@ -1,28 +1,38 @@
 package LeetCode75.easy;
+
 // 98.9%
 public class Task345_ReverseVowelsOfAString {
     public static String reverseVowels(String s) {
         char[] input = s.toCharArray();
 
         int startIndex = 0;
-        int endIndex = s.length();
+        int endIndex = s.length() - 1;
 
         while (true) {
-            while (!isVowel(input[startIndex++])) {
-                if (startIndex >= endIndex) {
+
+
+            for (int i = startIndex; i <= endIndex; i++) {
+                if (isVowel(input[i])) {
+                    startIndex = i;
                     break;
                 }
             }
+            for (int i = endIndex; i >= startIndex; i--) {
+                if (isVowel(input[i]) || i == startIndex) {
+                    endIndex = i;
+                    break;
+                }
+            }
+
             if (startIndex >= endIndex) {
                 break;
+            } else {
+                char temp = input[startIndex];
+                input[startIndex] = input[endIndex];
+                input[endIndex] = temp;
+                startIndex++;
+                endIndex--;
             }
-            while (!isVowel(input[--endIndex])) {
-
-            }
-
-            char temp = input[startIndex - 1];
-            input[startIndex - 1] = input[endIndex];
-            input[endIndex] = temp;
 
         }
 
