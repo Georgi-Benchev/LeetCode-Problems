db0d10391465ff29af3ee1a888d77941f25dfd3a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-12-21 23:56:58 +0200

    add main to project

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
+
+
+import LeetCode75.easy.Task605_CanPlaceFlowers;
+
 public class Main {
+    public static void main(String[] args) {
+   /*     double num = (double) 4 / 6;
+        int num1 = (int) Math.ceil((double) num);
+        System.out.println(num1);*/
+
+/*
+        System.out.println(Task1071_GreatestCommonDivisor.gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"));
+*/
+        System.out.println(Task605_CanPlaceFlowers.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
+    }
 }
