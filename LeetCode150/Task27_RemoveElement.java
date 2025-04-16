7bb33af80d323f5cbbbedda7ba678950b93f4f9f
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-04-16 21:13:35 +0300

    solved Task 27

---
+++ LeetCode150/Task27_RemoveElement.java
@@ -0,0 +1,15 @@
+package LeetCode150;
+
+// 100%
+public class Task27_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
